585963ac10b102a381699de175f636246302ff36
uyoung-jeong <jeong.uyoung@unist.ac.kr>
2022-04-27 07:33:50 +0000

    strying to make setup file

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="hhrnet-uyoung",
+    version="0.1",
+    author="Uyoung Jeong",
+    author_email="jeong.uyoung@unist.ac.kr",
+    description="custom modification of higherhrnet",
+    url="https://github.com/uyoung-jeong/HigherHRNet",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(),
+    python_requires=">=3.6",
+)
